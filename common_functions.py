745ef89ac4b365807e606682532c1cfc6ff307bf
CalebStock <calebstock91@gmail.com>
2021-08-15 19:02:32 -0500

    module added for commonly used functions.

--- common_functions.py
+++ common_functions.py
@@ -4,3 +4,9 @@ def quit(event):
     if event.type == pygame.QUIT:
         pygame.quit()
         exit()
+
+def sprite_parser(sheet,frame,width,height,color):
+    image=pygame.Surface((width,height)).convert_alpha()
+    image.blit(sheet,(0,0),((frame*width),0,width,height))
+    image.set_colorkey(color)
+    return image
