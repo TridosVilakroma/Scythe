a2a7383e1b454541fe29166c252b614d3b2890fc
CalebStock <calebstock91@gmail.com>
2021-08-22 14:56:39 -0500

    movment refined, blink step ability add

--- player_one.py
+++ player_one.py
@@ -1,6 +1,6 @@
-import pygame,common_functions
+import pygame,common_functions,time
 import operator
-from pygame import*
+#from pygame import*
 class PlayerOne(pygame.sprite.Sprite):
     def __init__(self,pos_x, pos_y):
         super().__init__()
@@ -15,7 +15,10 @@ class PlayerOne(pygame.sprite.Sprite):
         self.positionx=pos_x
         self.positiony=pos_y
         self.animate_speed=.09
-        self.speed=72
+        self.speed=180
+        self.blink_distance=90
+        self.blink_step_cooldown=.5
+        self.blink_time_ref=time.time()
     
         
     def animate_switch(self):
@@ -27,30 +30,109 @@ class PlayerOne(pygame.sprite.Sprite):
                 self.current_sprite=0
                 self.animating=False
         self.image=self.sprites[int(self.current_sprite)]
-
-
     def traverse(self,P1,delta):
         motionx=P1.get_axis(0)
         motiony=P1.get_axis(1)
+
         if motionx>.5:
-            if self.positionx>0:
+            if motiony>.5:
+                if self.positionx<968:
+                    if self.positiony<468:
+                        self.positionx+=(self.speed*delta)*.75
+                        self.positiony+=(self.speed*delta)*.75
+                        self.animate_switch()
+                        if P1.get_button(0):
+                            self.blink_step('right',P1)
+            elif motiony<-.5:
+                if self.positionx<968:
+                    if self.positiony>0:
+                        self.positionx+=(self.speed*delta)*.75
+                        self.positiony-=(self.speed*delta)*.75
+                        self.animate_switch()
+                        if P1.get_button(0):
+                            self.blink_step('right',P1)
+                       
+            elif self.positionx<968:
                 self.positionx+=self.speed*delta
                 self.animate_switch()
-        if motionx<-.5:
-            if self.positionx<1000:
+                if P1.get_button(0):
+                    self.blink_step('right',P1)
+        elif motionx<-.5:
+            if motiony>.5:
+                if self.positionx>0:
+                    if self.positiony<468:
+                        self.positionx-=(self.speed*delta)*.75
+                        self.positiony+=(self.speed*delta)*.75
+                        self.animate_switch()
+                        if P1.get_button(0):
+                            self.blink_step('left',P1)
+            elif motiony<-.5:
+                if self.positionx>0:
+                    if self.positiony>0:
+                        self.positionx-=(self.speed*delta)*.75
+                        self.positiony-=(self.speed*delta)*.75
+                        self.animate_switch()
+                        if P1.get_button(0):
+                            self.blink_step('left',P1)
+            elif self.positionx>0:
                 self.positionx-=self.speed*delta
                 self.animate_switch()
-        if motiony>.5:
-            if self.positiony>0:
+                if P1.get_button(0):
+                    self.blink_step('left',P1)
+        elif motiony>.5:
+            if self.positiony<468:
                 self.positiony+=self.speed*delta
                 self.animate_switch()
-        if motiony<-.5:
-            if self.positiony<500:
+                if P1.get_button(0):
+                    self.blink_step('down',P1)
+        elif motiony<-.5:
+            if self.positiony>0:
                 self.positiony-=self.speed*delta
                 self.animate_switch()
-
-
-
+                if P1.get_button(0):
+                    self.blink_step('up',P1)
+    def blink_step(self,direction,P1):
+        motionx=P1.get_axis(0)
+        motiony=P1.get_axis(1)
+        time_stamp=time.time()
+        if time_stamp>self.blink_time_ref:
+            self.blink_time_ref=time_stamp+self.blink_step_cooldown
+            if direction=='right':
+                if motiony>.5:
+                    self.positionx+=self.blink_distance/2
+                    self.positiony+=self.blink_distance/2
+                elif motiony<-.5:
+                    self.positionx+=self.blink_distance/2
+                    self.positiony-=self.blink_distance/2
+                else:
+                    self.positionx+=self.blink_distance
+            if direction=='left':
+                if motiony>.5:
+                    self.positionx-=self.blink_distance/2
+                    self.positiony+=self.blink_distance/2
+                elif motiony<-.5:
+                    self.positionx-=self.blink_distance/2
+                    self.positiony-=self.blink_distance/2
+                else:
+                    self.positionx-=self.blink_distance
+            if direction=='down':
+                if motionx>.5:
+                    self.positionx+=self.blink_distance/2
+                    self.positiony+=self.blink_distance/2
+                elif motionx<-.5:
+                    self.positionx-=self.blink_distance/2
+                    self.positiony+=self.blink_distance/2
+                else:
+                    self.positiony+=self.blink_distance
+            if direction=='up':
+                if motionx>.5:
+                    self.positionx+=self.blink_distance/2
+                    self.positiony-=self.blink_distance/2
+                elif motionx<-.5:
+                    self.positionx-=self.blink_distance/2
+                    self.positiony-=self.blink_distance/2
+                else:
+                    self.positiony-=self.blink_distance
         
     def update(self,P1,delta):
         self.animate()
