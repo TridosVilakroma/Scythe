ce5041abde6d05e60df8d13545bdb8df2de7c5ba
CalebStock <calebstock91@gmail.com>
2021-10-10 13:13:08 -0500

    made omnivines active

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ for i in range(10):
     i=enemies.Scarecrow()
     scarecrows.add(i)
     enemies.enemies.append(i)
-# for i in range(5):
-#     i=enemies.Omnivine()
-#     scarecrows.add(i)
+for i in range(5):
+    i=enemies.Omnivine()
+    scarecrows.add(i)
 player.scarecrows=scarecrows
 
 #player binding
